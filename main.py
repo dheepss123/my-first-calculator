ca79748663cb179a889dbe1d043445cfa516b5b7
Dheepss123 <dheepss321@gmail.com>
2023-09-05 17:52:52 +0700

    add file calculator

---
+++ main.py
@@ -0,0 +1,42 @@
+# Definisi fungsi
+def kalkulator():
+    while True:
+        print("Pilihan operasi matematika yang tersedia: ")
+        print("1. Penjumlahan (+)")
+        print("2. Pengurangan (-)")
+        print("3. Perkalian (*)")
+        print("4. Pembagian (/)")
+        print("5. Keluar")
+
+        # Meminta user memilih operasi perhitungan
+        pilihan = input("Masukkan bilangan operasi yang anda inginkan (1/2/3/4/5): ")
+
+        if pilihan == '5':
+            print("Terima kasih telah menggunakan kalkulator.")
+            break  # Keluar dari loop jika pengguna memilih keluar
+
+        if pilihan in ('1', '2', '3', '4'):
+            angka1 = int(input("Masukkan angka pertama: "))
+            angka2 = int(input("Masukkan angka kedua: "))
+
+            if pilihan == '1':
+                hasil = angka1 + angka2
+            elif pilihan == '2':
+                hasil = angka1 - angka2
+            elif pilihan == '3':
+                hasil = angka1 * angka2
+            elif pilihan == '4':
+                # Memeriksa pembagian dengan nol
+                if angka2 != 0:
+                    hasil = angka1 / angka2
+                else:
+                    hasil = "Tidak dapat membagi oleh nol"
+
+            # Menampilkan hasil
+            print("Hasil: ", hasil)
+            
+        else:
+            print("Pilihan operasi tidak valid")
+
+# Memanggil fungsi kalkulator
+kalkulator()
